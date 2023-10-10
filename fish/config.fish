2a15ed46a5765303d5dd7a6b53b61aafe7a788a4
rasib0 <rasibnadeem101@gmail.com>
2023-10-11 00:34:21 +0500

    added fish

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias vim='nvim'
+alias np='cd ~/projects && nvim'
+alias config='cd ~/.config'
+alias home='cd ~'
+alias cls='clear'
+alias notes='cd storage/downloads/Vault/'
+alias books='cd storage/downloads/Books/'
