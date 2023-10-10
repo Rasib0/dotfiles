7242c4ba9c6f555b84a3670cb6aebfc8661778e7
Rasib0 <rasibnadeem101@gmail.com>
2023-10-11 02:22:00 +0500

    doing

--- fish/config.fish
+++ fish/config.fish
@@ -5,18 +5,18 @@ end
 alias booksm='cd storage/downloads/Books/'
 alias notesm='cd storage/downloads/Vault/'
 
-alias books="cd ~/Projects/Books/ ; $EDITOR"
-alias notes="cd ~/Projects/Vault/ ; $EDITOR"
-alias np='cd ~/Projects && tmux && nvim'
-alias conf='cd ~/.config'
+alias books="cd ~/Projects/Books/"
+alias notes="cd ~/Projects/Vault/ && $EDITOR"
+alias conf='cd ~/.config/'
+alias proj ='cd ~/Projects/'
 alias home='cd ~'
 alias cls="clear"
 alias vim="nvim"
-alias i3config="$EDITOR ~/.config/i3/config"
 alias nvimconfig="$EDITOR ~/.config/nvim/"
+alias fishconfig="$EDITOR ~/.config/fish/"
 alias tmuxconfig="$EDITOR ~/.tmux.conf"
+alias i3config="$EDITOR ~/.config/i3/config"
 alias swayconfig="$EDITOR ~/.config/sway/"
-
 
 function cpprun
     function _cppcompile
