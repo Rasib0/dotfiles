4d37df4e643c99fd6ae1fc8ba8abefb17050d712
Rasib0 <rasibnadeem101@gmail.com>
2023-10-11 02:16:16 +0500

    tried to sync mobile and pc fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,28 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias vim='nvim'
-alias np='cd ~/projects && nvim'
-alias config='cd ~/.config'
+alias booksm='cd storage/downloads/Books/'
+alias notesm='cd storage/downloads/Vault/'
+
+alias books="cd ~/Projects/Books/ ; $EDITOR"
+alias notes="cd ~/Projects/Vault/ ; $EDITOR"
+alias np='cd ~/Projects && tmux && nvim'
+alias conf='cd ~/.config'
 alias home='cd ~'
-alias cls='clear'
-alias notes='cd storage/downloads/Vault/'
-alias books='cd storage/downloads/Books/'
+alias cls="clear"
+alias vim="nvim"
+alias i3config="$EDITOR ~/.config/i3/config"
+alias nvimconfig="$EDITOR ~/.config/nvim/"
+alias tmuxconfig="$EDITOR ~/.tmux.conf"
+alias swayconfig="$EDITOR ~/.config/sway/"
+
+
+function cpprun
+    function _cppcompile
+        set filename $argv[1]
+        set output_file (string replace -r '\.cpp$' '.out' $filename)
+        clang++ $filename -o $output_file
+        ./$output_file <input.txt
+    end
+    _cppcompile $argv[1]
+end
