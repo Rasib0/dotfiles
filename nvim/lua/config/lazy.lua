bee11e3d44e847e421e5cd468d5a9570d663ab53
Rasib0 <rasibnadeem101@gmail.com>
2023-12-08 23:11:40 +0500

    removed extra plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -51,6 +51,6 @@ require("lazy").setup({
 })
 
 -- This settings lets clangd to work with utf-16 files
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.offsetEncoding = { "utf-16" }
-require("lspconfig").clangd.setup({ capabilities = capabilities })
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.offsetEncoding = { "utf-16" }
+-- require("lspconfig").clangd.setup({ capabilities = capabilities })
