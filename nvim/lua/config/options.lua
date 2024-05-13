5cdd2f3ea9762dba952307b7c4ca7a92f7986c49
Rasib0 <rasibnadeem101@gmail.com>
2024-05-13 15:57:04 +0500

    commented out wsl specific clipboard issue

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,20 +2,21 @@
 -- Add any additional options here
 
 -- Added clipboard support for wsl2 client
-if vim.fn.has("wsl") then
-  vim.g.clipboard = {
-    name = "win_clipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = "powershell.exe Get-Clipboard",
-      ["*"] = "powershell.exe Get-Clipboard",
-    },
-    cache_enabled = 0,
-  }
 
-  vim.keymap.set({ "n", "v" }, "y", '"+y', { noremap = true, silent = true })
-  vim.keymap.set({ "n", "v" }, "p", '"+p', { noremap = true, silent = true })
-end
+-- if vim.fn.has("wsl") then
+--   vim.g.clipboard = {
+--     name = "win_clipboard",
+--     copy = {
+--       ["+"] = "clip.exe",
+--       ["*"] = "clip.exe",
+--     },
+--     paste = {
+--       ["+"] = "powershell.exe Get-Clipboard",
+--       ["*"] = "powershell.exe Get-Clipboard",
+--     },
+--     cache_enabled = 0,
+--   }
+--
+--   vim.keymap.set({ "n", "v" }, "y", '"+y', { noremap = true, silent = true })
+--   vim.keymap.set({ "n", "v" }, "p", '"+p', { noremap = true, silent = true })
+-- end
