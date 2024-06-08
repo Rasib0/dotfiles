80f805c8ed308c3e08b5ff2850625bc10c8bc489
rasibn <rasibnadeem101@gmail.com>
2024-06-09 02:03:46 +0500

    fixed fish and added helix

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,6 @@ alias notesm='cd ~/storage/shared/Documents/Vault/'
 
 alias books="cd ~/Projects/Books/"
 alias notes="cd ~/Projects/Vault/ && $EDITOR"
-alias conf='cd ~/.config/'
-alias proj ='cd ~/Projects/'
-alias home='cd ~'
 alias cls="clear"
 alias nvimconfig="$EDITOR ~/.config/nvim/"
 alias fishconfig="$EDITOR ~/.config/fish/"
@@ -20,14 +17,5 @@ alias zshconfig="$EDITOR ~/.zshrc"
 alias i3config="$EDITOR ~/.config/i3/config"
 alias swayconfig="$EDITOR ~/.config/sway/"
 alias reader="zathura"
-alias tvim ="tmux & nvim"
-
-function clangr
-    function _cppcompile
-        set filename $argv[1]
-        set output_file (string replace -r '\.cpp$' '.out' $filename)
-        clang++ $filename -o a.out
-        ./a.out <input.txt
-    end
-    _cppcompile $argv[1]
-end
+alias tvim="tmux & $EDITOR"
+alias hx="helix"
