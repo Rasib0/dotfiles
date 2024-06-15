5f99d55b736ebfea3c6a4a00b359deda0fc3623a
rasibn <rasibnadeem101@gmail.com>
2024-06-15 14:40:37 +0500

    nvim: Removed Deprecated function in autocmd.lua

--- components/nvim/lua/config/autocmds.lua
+++ components/nvim/lua/config/autocmds.lua
@@ -2,14 +2,10 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.cmd("silent! Copilot disable")
-
-vim.api.nvim_exec(
-  [[
+vim.cmd("silent! Copilot disable")
+vim.cmd([[
  augroup RustSettings
      autocmd!
      autocmd FileType rust inoremap ' '
  augroup END
- ]],
-  false
-)
+ ]])
