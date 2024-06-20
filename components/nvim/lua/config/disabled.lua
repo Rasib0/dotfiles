a5f98bb026d06b634ea6fa5347b0cf7220004d94
rasibn <rasibnadeem101@gmail.com>
2024-06-20 10:45:44 +0500

    Updated neovim; added disabled.lua;

---
+++ components/nvim/lua/config/disabled.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    otps = {
+      inlay_hints = { enabled = false },
+    },
+  },
+  {
+    "folke/noice.nvim",
+    config = {
+      cmdline = {
+        enabled = false,
+      },
+    },
+  },
+}
